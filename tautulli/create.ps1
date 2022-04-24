54c9965c5a7c9e40440a55b9683b6222bee9afe8
Evelyn Helton <eve@fallendusk.com>
2022-04-24 10:45:24 -0400

    Fix tautulli's create script

--- tautulli/create.ps1
+++ tautulli/create.ps1
@@ -5,7 +5,7 @@ docker run -d `
     --hostname tautulli `
     --name tautulli `
     -v c:\services\tautulli:c:\config `
-    -v "c:\services\plex\plex media server\logs":c:\plexlogs `
-    -p 8080:8080 `
+    -v "c:\services\plex\plex media server\logs:c:\plexlogs" `
+    -p 8181:8181 `
     --restart=always `
 bootyberry/tautulli:latest
